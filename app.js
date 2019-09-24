d03ad4602bd717f38d6e69db66a8e4dab88070fc
Divyanshu Saini <bat2268@gmail.com>
2019-09-24 09:00:14 +0530

    added routes for filters

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 
 const app = express();
+const URI = process.env.MONGODB_URL;
+
+mongoose.connect(URI, {
+  poolSize: 10000,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+
+db.on('error', (error) => console.error(error))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -14,7 +26,9 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,77 @@
 const express = require('express');
 const router = express.Router();
+const IonDoc = require('../models/IonModel');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* Ping route. */
+router.get('/', (req, res, next) => {
+  res.json({'server status ':'Ok'});
+});
+
+/* To fetch available Temperature */
+router.get('/temp', (req, res, next) => {
+  IonDoc.aggregate([{
+    $group: {
+      "_id": "$val"
+    }
+  }]).exec((err, temp) => {
+    if (!err) {
+      res.json(temp)
+    } else {
+      res.json(err)
+    }
+  });
+});
+
+/* To fetch availabel Year */
+router.get('/year', (req, res, next) => {
+  IonDoc.aggregate([{
+    $group: {
+      "_id": "$year"
+    }
+  }]).exec((err, year) => {
+    if (!err) {
+      res.json(year)
+    } else {
+      res.json(err)
+    }
+  });
+});
+
+/* To fetch data based on temperature for the given year*/
+router.get('/filter/:year/:temp', (req, res, next) => {
+  let year = parseInt(req.params.year);
+  let temperature = parseInt(req.params.temp);
+
+  IonDoc.aggregate([{
+      $match: {
+        "val": temperature,
+        "year": year
+      }
+    },
+    {
+      $group: {
+        "_id": {
+          Month: "$month",
+          Temperature: "$val"
+        },
+        count: {
+          $sum: 1
+        }
+      }
+    }
+  ]).exec((err, filterData) => {
+    if (!err) {
+      res.json(filterData)
+    } else {
+      res.json(err)
+    }
+  });
+})
+
+/* To upload logs in mongodb */
+router.post('/upload/logs', (req, res, next) => {
+  console.log(req.body)
+  res.send('uppload success')
 });
 
 module.exports = router;
